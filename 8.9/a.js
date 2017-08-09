13190ed64a94b92ebc197fca7daa99ef288e0acd
yuanshijing <826774822@qq.com>
2017-08-09 07:50:34 +0000

    complet 8.9

---
+++ 8.9/a.js
@@ -0,0 +1,10 @@
+function build_array(n){
+    let array = [0,1];
+    for (let i = 2;i<n;i++) {
+        array.push(array[i-1] + array[i-2]);
+    }
+    console.log(array);
+    console.log(array[n-1]);
+}
+
+build_array(15);
