09a3f0494e6883fce4c73cc5996d569b609cc421
yuanshijing <826774822@qq.com>
2017-08-12 07:41:21 +0000

    complete

---
+++ 8.12/main.js
@@ -0,0 +1,19 @@
+function flowernum() {
+    let result = [];
+    for (let i = 100; i <= 999; i++) {
+        let dic = i.toString().split('');
+        let sum = 0;
+        for(let num of dic){
+            sum += Math.pow(parseInt(num), 3);
+        }
+        if (i == sum) {
+            result.push(i);
+        }
+    }
+    return result;
+}
+
+console.log(flowernum());
+
+
+
