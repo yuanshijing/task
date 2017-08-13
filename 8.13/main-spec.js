de0cdea80491f491418ca642a46221565483c17e
yuanshijing <826774822@qq.com>
2017-08-13 20:35:16 +0800

    'complete'

--- 8.13/main-spec.js
+++ 8.13/main-spec.js
@@ -1,19 +1,19 @@
 'use strict'
-const findMidian =require('./main.js');
+const findMedian =require('./main.js');
 
-descrbe('midian',() => {
+describe('median',() => {
 
     it('数组长度为奇数',() => {
         //GIVEN 
         let arr =[1,2,3,4,5,];
         //WHEN
-        let result = findMidian(arr);
+        let result = findMedian(arr);
         //THEN
         expect(result).toEqual(3);
     })
     it('数组长度为偶数',()=>{
         let arr = [1,2,3,4,5,6];
-        let result = findMidian(arr);
+        let result = findMedian(arr);
         expect(result).toEqual(3.5);
     })
 })
