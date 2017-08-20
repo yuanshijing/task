0b352b770effd297ec7684a34061b465c11f0802
yuanshijing <826774822@qq.com>
2017-08-21 01:08:51 +0800

    'complete'

---
+++ 8.20/main.js
@@ -0,0 +1,27 @@
+function maxTimes(str) {
+    let cnt = {};
+    let dic = [];
+    let arr = str.split('');
+    arr.forEach((i) =>{
+        if (cnt.hasOwnProperty(i))
+            cnt[i]++;
+        else {
+            cnt[i] = 1;
+            dic.push(i);
+        }
+    });
+    let max = cnt[dic[0]];
+    dic.forEach((i) =>{
+        if (cnt[i] > max)
+            max = cnt[i];
+    });
+    let result = {};
+    dic.forEach((i) =>{
+        if (cnt[i] == max)
+            result[i] = max;
+    });
+    console.log(max);
+    return result;
+}
+console.log(maxTimes('aaaaabbdbdbb'));
+console.log(maxTimes('abcsbaddbizdbas'));
